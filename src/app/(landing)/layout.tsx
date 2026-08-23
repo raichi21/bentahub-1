7e52ee514107500a38f99b658601ae1b7afaff42
raichi21 <raichixiao21@gmail.com>
2026-08-23 12:29:40 +0800

    feat: create landing page layout with metadata and navigation bar

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next"
 import { Navbar } from "@/features/landing"
 
 export const metadata: Metadata = {
-  title: "BentaHub | Your Neighborhood Sari-Sari Store, Now Digital",
-  description: "BentaHub brings your local stores online. Browse real-time inventory, reserve items, and pick them up when ready.",
+  title: "BentaHub | Your Neighborhood Sari-Sari Store, Are now online store",
+  description: "BentaHub is a platform that helps you to reserve items from your Sari-Sari Store you can reserve items, and pick them up at your convenient time.",
 }
 
 export default function LandingLayout({
